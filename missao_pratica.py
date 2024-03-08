f59b18369c8e26178375de9d2f01220f4db0b333
luiz-linux <luizcmoficial@gmail.com>
2024-03-08 12:13:11 -0300

    conversao de dados

--- missao_pratica.py
+++ missao_pratica.py
@@ -1,15 +1,25 @@
 from faker import Faker
 
+
 fake = Faker('pt-BR')
 names = []
 
+# Gerando 20 nomes brasileiros aleatórios e número aleatório para cada nome
 for i in range(20):
     names.append([fake.name(), str(fake.random_int(1,10))])
 
-print(names)
+print(names, '\n')
 
+# Criando ou abrindo arquivo.txt e escrevendo os 20 nomes e o número aleatório no arquivo.txt
 with open('arquivo.txt', 'w') as arquivo:
-    for j in names:
-        arquivo.write(','.join(j))
-        arquivo.write('\n')
+    [arquivo.write(','.join(j) + '\n') for j in names]
 
+# Recuperando os dados no arquivo.txt
+names_graph = []
+with open('arquivo.txt', 'r') as arquivo:
+    [names_graph.append((linha.strip().split(','))) for linha in arquivo]
+    
+    for elemento in names_graph:
+        elemento[1] = int(elemento[1])
+
+print(names_graph)
