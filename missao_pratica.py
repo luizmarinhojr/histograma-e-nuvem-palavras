e3f77865bd8216b21872acda36392e5479ccd504
luiz-linux <luizcmoficial@gmail.com>
2024-03-08 14:25:26 -0300

    graphic-view

--- missao_pratica.py
+++ missao_pratica.py
@@ -1,25 +1,41 @@
 from faker import Faker
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 fake = Faker('pt-BR')
-names = []
 
 # Gerando 20 nomes brasileiros aleatórios e número aleatório para cada nome
+names = []
 for i in range(20):
     names.append([fake.name(), str(fake.random_int(1,10))])
 
 print(names, '\n')
+
+# --------------------------------------------------------------------
+
 
 # Criando ou abrindo arquivo.txt e escrevendo os 20 nomes e o número aleatório no arquivo.txt
 with open('arquivo.txt', 'w') as arquivo:
     [arquivo.write(','.join(j) + '\n') for j in names]
 
 # Recuperando os dados no arquivo.txt
-names_graph = []
+nomes_arquivo = []
 with open('arquivo.txt', 'r') as arquivo:
-    [names_graph.append((linha.strip().split(','))) for linha in arquivo]
-    
-    for elemento in names_graph:
-        elemento[1] = int(elemento[1])
+    [nomes_arquivo.append((linha.strip().split(','))) for linha in arquivo]
+    print(nomes_arquivo)
 
-print(names_graph)
+pontuacao = []
+for nome in nomes_arquivo:
+    pontuacao.append(int(nome[1]))
+
+print(pontuacao)
+
+# total = 10
+
+plt.hist(pontuacao, 10, density=True, facecolor='green', alpha=0.75)
+
+plt.xlabel('Valores')
+plt.ylabel('Probabilidade')
+plt.title('Histograma dos valores')
+plt.grid(True)
+plt.show()
